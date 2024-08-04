0fd55b77ceb3b5ab8a9f2545af1253a8e338611c
Vishnu P Raj <vshnupraj@gmail.com>
2024-08-04 13:43:37 +0530

    update existing file

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <Home />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/Menu' element={<Menu />} />
